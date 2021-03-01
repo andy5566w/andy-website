a99b84522d8736f434a6038cf8cd0de44091ebba
andy tsai <andy5566w@gmail.com>
2021-03-01 23:14:06 +0800

    fix: 修復部署錯誤

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,32 +6,32 @@ class Footer extends Component {
       <footer className="footer">
         <nav className="nav">
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Find your dream home
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Request proposal
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Download home planner
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Contact us
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Submit your property
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Come work with us!
             </a>
           </li>
